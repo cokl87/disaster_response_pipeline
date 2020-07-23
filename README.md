f28bba701425b3818048aae0110af2619b6d807f
Kornel <cornelius.klieber@web.de>
2020-07-23 11:17:56 +0200

    initializing of project

---
+++ README.md
@@ -0,0 +1,38 @@
+# Disaster Response Pipeline
+
+### Table of Contents
+1. [Installation](#installation)
+2. [Project Motivation](#project-motivation)
+3. [File Descriptions](#file-descriptions)
+4. [Results](#results)
+5. [Licensing, Authors, Acknowledgements](#licensing,-authors,-acknowledgements)
+
+
+## Installation
+<!--- 
+For the analysis python3.8 was used ogether with a jupyther notebook. In order to get the project running you'll need also some 3rd party libraries which are all mentioned in the [requirements.txt](./requirements.txt) file, included in this project which can be used for generating a virtualenv.
+-->
+
+## Project Motivation
+
+## File Descriptions
+<!---
+There are 2 notebooks available here to showcase work related to the above questions.
+* [screening_of_data.ipynb](./screening_of_data.ipynb): This notebook was used to get a overview of the available survey-data and checking if the survey data between years has a similar form. 'What quuestions can be answered?', 'What data can be used?'
+* [analysis.ipynb](./analysis.ipynb): This notebook was used for actually answering the questions abouve
+
+There are some adition .py files which classes and functions used in the notebooks:
+* [functions.py](./functions.py) some generalized functions used in the analysis
+* [log_config.py](./log_config.py) basic configuration of a logger used
+* [plotter.py](./plotter.py) plotter class used for generating plots for the report
+-->
+
+## Results
+<!---
+Are shortly discussed in markdown cells within the notebooks. The main findings are discussed in this [post]().
+-->
+
+## Licensing, Authors, Acknowledgements
+<!---
+Credit for the data must be given to Stack Overflow. The full data and it's description can be found [here](https://insights.stackoverflow.com/survey). You may use the code of this porject as you like.
+-->
---
+++ source/log_config.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""
+log_config.py contains a function for the standard configuration of the logging-module.
+Currently a logger for stdout-stream and a default for logging into files and stdout are configured.
+
+created:
+author: kornel
+"""
+
+import logging
+from logging.config import dictConfig
+import os.path
+
+
+def get_configured_logger(name, filename=None, verbose=True):
+    """
+    function configures logging and returns the requested logger
+
+    Parameters:
+    -----------
+    name: str
+        name of the logger to be returned
+    filename: str
+        path of the file where the logging shall take place
+    verbose: bool
+        which formatter to use (verbose messages or taciturn)
+
+    Returns:
+    --------
+    logging.Logger
+
+    """
+
+    verbosity = 'verbose' if verbose else 'taciturn'
+    if not filename:
+        filename = os.path.join(os.getcwd(), 'log.txt')
+
+    config_dict = {
+        'version': 1,
+        'formatters': {
+            'verbose': {'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        'datefmt': '%Y-%m-%d %H:%M:%S'},
+            'taciturn': {'format': '%(message)s'},
+        },
+        'handlers': {
+            'stdout': {
+                'level': 'DEBUG',
+                'formatter': verbosity,
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',
+            },
+            'file_handler': {
+                'level': 'WARNING',
+                'formatter': verbosity,
+                'filename': filename,
+                'mode': 'a',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'maxBytes': 102400,
+                'backupCount': 3
+            }
+        },
+        'loggers': {
+            'default': {
+                'level': 'INFO',
+                'handlers': ['stdout', 'file_handler'],
+            },
+            'stdout': {
+                'level': 'INFO',
+                'handlers': ['stdout']
+            },
+        },
+        'disable_existing_loggers': False
+    }
+
+    dictConfig(config_dict)
+    return logging.getLogger(name)
